6dfae31b0626d9564fa59f2aa70b6b7c72024088
pavinane <pavimegan77@gmail.com>
2022-09-30 10:17:57 +0530

    transaction process

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("this is my first smart contract");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s of waved", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("%d of total wave", totalWaves);
+        return totalWaves;
     }
 }
